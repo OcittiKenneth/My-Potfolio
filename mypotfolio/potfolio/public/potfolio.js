6630793dbe6a98c0c104579cd21d4e8553d36ad2
OcittiKenneth <kennethocitti95@gmail.com>
2019-09-13 15:42:09 +0300

    pushing to github

---
+++ mypotfolio/potfolio/public/potfolio.js
@@ -0,0 +1,42 @@
+const validate = (event) => {
+
+    // console.log("yap.....");
+
+    // name
+    const nameInput = document.querySelector(".name");
+    const reName = /^[A-Za-z\s]+$/;
+
+    if (!reName.test(nameInput.value)) {
+        document.querySelector("#nameSpan").innerHTML = "Name is Required";
+        nameInput.style.border = "1px solid red";
+
+    } else {
+        document.querySelector("#nameSpan").innerHTML = "";
+        nameInput.style.border = "1px solid green";
+
+    }
+
+    // email
+    const emailInput = document.getElementById("email");
+    if (emailInput.value == '') {
+        document.getElementById("emailSpan").innerHTML = "Email is Required";
+        emailInput.style.border = "1px solid red";
+
+    } else {
+        document.getElementById("emailSpan").innerHTML = "";
+        emailInput.style.border = "1px solid green";
+    }
+
+    // subject
+    const subject = document.getElementById("subject");
+    const reSubject = /^[A-Za-z\s]+$/;
+    if (!reSubject.test(subject.value)) {
+        document.getElementById("subId").innerHTML = "Put the subject please";
+        subject.style.border = "1px solid red";;
+    } else {
+        document.getElementById("subId").innerHTML = "";
+        subject.style.border = "1px solid green";
+    }
+    return false;
+
+}
